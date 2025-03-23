b9a7a8abbda8eb1ef0cd57739178918e23dfd550
lifeadventurer <life0adventurer@gmail.com>
2025-03-23 22:30:00 +0800

    add data/fenwick.cpp

---
+++ data/fenwick.cpp
@@ -0,0 +1,30 @@
+template <typename T>
+class FenwickTree {
+ public:
+  int n;
+  vector<T> fenw;
+
+  FenwickTree(int _n) : n(_n) {
+    fenw.assign(n, 0);
+  }
+
+  int lowbit(int x) {
+    return x & -x;
+  }
+
+  void modify(int x, T val) {
+    while (x < n) {
+      fenw[x] += val;
+      x += lowbit(x)){
+    }
+  }
+
+  T query(int x) {
+    T val{};
+    while (x > 0) {
+      val += fenw[x];
+      x -= lowbit(x)
+    }
+    return val;
+  }
+};
